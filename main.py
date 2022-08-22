7346818b57aa57800374cf721e6a34ac8c6dc203
FestusAmoako <nanaamaoako@456@gmail.com>
2022-08-22 11:14:15 -0700

    Finished v2.0

--- main.py
+++ main.py
@@ -1,49 +1,64 @@
 from colours import *
 from random import randint
 
+
 # welcome screen message
-print(BLUE, '****welcome to Your world of Guessing****')
-user_input = int(input((White + 'choose your level\n'
-                                '1. normal (1-50)\n'
-                                '2. intermediate(1-200)\n'
-                                '3. hard(1-500)\n'
-                                '4. exit application\n'
-                                'Enter request: ')))
+def welcome_screen():
+    print(BLUE, '****welcome to Your world of Guessing****')
+    user_input = int(input((White + 'choose your level\n'
+                                    '1. normal (1-50)\n'
+                                    '2. intermediate(1-200)\n'
+                                    '3. hard(1-500)\n'
+                                    '4. exit application\n\n'
+                                    'Enter request: ')))
+    # calling a function and passing user_input to it
+    guess_range = user_option(user_input)
+    if guess_range != 0:
+        generate_random_number(guess_range)
+
 
 #  user input
-guess_range = 0
-if user_input == 1:
-    guess_range = 50
-elif user_input == 2:
-    guess_range = 200
-elif user_input == 3:
-    guess_range = 500
-elif user_input == 4:
-    print('**** Thanks For Using Our App , Hope To See You Again')
-    exit(1)
-else:
-    print(Red + 'invalid Request, Try Again')
+def user_option(user_input):
+    guess_range = 0
+    if user_input == 1:
+        guess_range = 50
+    elif user_input == 2:
+        guess_range = 200
+    elif user_input == 3:
+        guess_range = 500
+    elif user_input == 4:
+        print('**** Thanks For Using Our App , Hope To See You Again****')
+        exit(1)
+    else:
+        print(Red, 'invalid Request, Try Again')
+        welcome_screen()
+    return guess_range
 
-# Generate Random number between certain numbers,
-generate_number = randint(1, guess_range)
 
-# user chances
-user_chances = 4
+def generate_random_number(guess_range):
+    # Generate Random number between certain numbers,
+    generate_number = randint(1, guess_range)
 
-for i in range(4):
-    user_guess = int(input('provide Your Guess Number: '))
+    # user chances
+    user_chances = 4
 
-    # determine if the guess is correct
-    if user_guess == generate_number:
-        print(Green, f'Hurray!!!, Your Guess is Correct')
-        break
-    elif user_guess > generate_number:
-        print(Yellow, 'You Guessed Too high Try Again')
-    elif user_guess < generate_number:
-        print(Yellow, 'you  Guessed Too low ')
-    # reducing user chance and prompt user chances left
-    user_chances -= 1
-    print(f'number of chances left is {user_chances}')
-# inform user of the correct guess number
-if user_chances == 0:
-    print(Red,f'the correct guess is {generate_number}')
+    for i in range(4):
+        user_guess = int(input('provide Your Guess Number: '))
+
+        # determine if the guess is correct
+        if user_guess == generate_number:
+            print(Green, f'Hurray!!!, Your Guess is Correct')
+            break
+        elif user_guess > generate_number:
+            print(Yellow, 'You Guessed Too high Try Again')
+        elif user_guess < generate_number:
+            print(Yellow, 'you  Guessed Too low ')
+        # reducing user chance and prompt user chances left
+        user_chances -= 1
+        print(f'number of chances left is {user_chances}')
+    # inform user of the correct guess number
+    if user_chances == 0:
+        print(Red, f'the correct guess is {generate_number}')
+
+
+welcome_screen()
